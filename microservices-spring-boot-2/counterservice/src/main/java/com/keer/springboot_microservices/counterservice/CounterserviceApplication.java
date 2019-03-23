156be1b7e8189f0c80b088fdc8ce6604dbe16026
keer2345 <keer2345@gmail.com>
2019-03-23 16:06:50 +0000

    add docker-compose.yml to counterservice

--- microservices-spring-boot-2/counterservice/src/main/java/com/keer/springboot_microservices/counterservice/CounterserviceApplication.java
+++ microservices-spring-boot-2/counterservice/src/main/java/com/keer/springboot_microservices/counterservice/CounterserviceApplication.java
@@ -2,8 +2,12 @@ package com.keer.springboot_microservices.counterservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableRetry
 public class CounterserviceApplication {
 
 	public static void main(String[] args) {
---
+++ microservices-spring-boot-2/counterservice/src/main/java/com/keer/springboot_microservices/counterservice/api/CounterController.java
@@ -0,0 +1,22 @@
+package com.keer.springboot_microservices.counterservice.api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RefreshScope
+public class CounterController {
+    private AtomicLong count = new AtomicLong(0L);
+
+    @Value(value = "${counter.prefixMessage}")
+    private String prefixMessage;
+
+    @RequestMapping("/count")
+    public String getCount() {
+        return prefixMessage + count.getAndIncrement();
+    }
+}
